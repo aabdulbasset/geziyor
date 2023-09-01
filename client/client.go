a1f2fa9a4a69195b2547725ccf95cc2d76aceba3
aabdulbasset <aabdulbasset5@gmail.com>
2023-09-01 13:00:49 +0300

    Changed http client options

--- client/client.go
+++ client/client.go
@@ -74,16 +74,16 @@ func NewClient(opt *Options) *Client {
 		Transport: &http.Transport{
 			Proxy: proxyFunction,
 			DialContext: (&net.Dialer{
-				Timeout:   30 * time.Second,
-				KeepAlive: 30 * time.Second,
+				Timeout:   15 * time.Second,
+				KeepAlive: 15 * time.Second,
 				DualStack: true,
 			}).DialContext,
 			ForceAttemptHTTP2:     true,
 			MaxIdleConns:          0,    // Default: 100
 			MaxIdleConnsPerHost:   1000, // Default: 2
-			IdleConnTimeout:       90 * time.Second,
+			IdleConnTimeout:       15 * time.Second,
 			TLSHandshakeTimeout:   10 * time.Second,
-			ExpectContinueTimeout: 1 * time.Second,
+			ExpectContinueTimeout: 2 * time.Second,
 		},
 		Timeout: time.Second * 180, // Google's timeout
 	}
