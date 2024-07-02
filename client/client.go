9247cec024dde807d5a12ba53b2c2f8bfdd9cb7d
aabdulbasset <aabdulbasset5@gmail.com>
2024-07-02 21:39:51 +0300

    [feat]

--- client/client.go
+++ client/client.go
@@ -24,6 +24,8 @@ import (
 	"github.com/chromedp/cdproto/network"
 	"github.com/chromedp/chromedp"
 	"github.com/imroc/req/v3"
+	"github.com/imroc/req/v3/http2"
+	utls "github.com/refraction-networking/utls"
 )
 
 var (
@@ -154,7 +156,9 @@ func (c *Client) DoRequest(req *Request) (resp *Response, err error) {
 // doRequestClient is a simple wrapper to read response according to options.
 func (c *Client) doRequestClient(req *Request) (*Response, error) {
 	// Do request
-	clonedClient := c.Clone()
+	clonedClient := &MyClient{
+		Client: c.Clone(),
+	}
 	request := clonedClient.R()
 	request.RawRequest = req.Request
 	if req.Header.Get("user-agent") != "" {
@@ -221,6 +225,82 @@ func (c *Client) doRequestClient(req *Request) (*Response, error) {
 	}
 
 	return &response, nil
+}
+
+type MyClient struct {
+	*req.Client
+}
+
+func NewMyClient() *MyClient {
+	return &MyClient{
+		Client: req.C(),
+	}
+}
+func (c *MyClient) ImpersonateChrome120() *MyClient {
+	chromeHttp2Settings := []http2.Setting{
+		{
+			ID:  http2.SettingHeaderTableSize,
+			Val: 65536,
+		},
+		{
+			ID:  http2.SettingEnablePush,
+			Val: 0,
+		},
+
+		{
+			ID:  http2.SettingInitialWindowSize,
+			Val: 6291456,
+		},
+		{
+			ID:  http2.SettingMaxHeaderListSize,
+			Val: 262144,
+		},
+	}
+	c.SetTLSFingerprint(utls.HelloChrome_120_PQ).
+		SetHTTP2SettingsFrame(chromeHttp2Settings...).
+		SetHTTP2ConnectionFlow(15663105).
+		SetCommonPseudoHeaderOder([]string{
+			":method",
+			":authority",
+			":scheme",
+			":path",
+		}...).
+		SetCommonHeaderOrder([]string{
+			"sec-ch-ua",
+			"dnt",
+			"upgrade-insecure-requests",
+			"user-agent",
+			"accept",
+			"sec-fetch-site",
+			"sec-fetch-mode",
+			"sec-fetch-user",
+			"sec-fetch-dest",
+			"accept-encoding",
+			"accept-language",
+			"priority",
+		}...).
+		SetCommonHeaders(map[string]string{
+			"cache-control":             "max-age=0",
+			"sec-ch-ua":                 `"Not/A)Brand";v="8", "Chromium";v="126", "Google Chrome";v="126"`,
+			"dnt":                       " 1",
+			"upgrade-insecure-requests": " 1",
+			"user-agent":                " Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36",
+			"accept":                    " text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+			"sec-fetch-site":            " none",
+			"sec-fetch-mode":            " navigate",
+			"sec-fetch-user":            " ?1",
+			"sec-fetch-dest":            " document",
+			"accept-encoding":           " gzip, deflate, br, zstd",
+			"accept-language":           " en-US,en;q=0.9",
+			"priority":                  " u=0, i",
+		}).
+		SetHTTP2HeaderPriority(http2.PriorityParam{
+			StreamDep: 0,
+			Exclusive: true,
+			Weight:    255,
+		})
+
+	return c
 }
 
 // doRequestChrome opens up a new chrome instance and makes request
